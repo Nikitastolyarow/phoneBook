36f6127ea7044a4f130ae5ee06002314bbc41a46
Nikitastolyarow <nikita.stolyarow2002@yandex.ru>
2024-09-02 21:01:00 +0300

    Stub and tests for add

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -1,2 +1,8 @@
+import java.util.HashMap;
+
 public class PhoneBook {
+    HashMap<String, String> namesAndNumber = new HashMap<>();
+    public static int add (String name, String number){
+        return 0;
+    }
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,2 +1,12 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class PhoneBookTest {
+    @Test
+    public void testAdd() {
+        PhoneBook phoneBook = new PhoneBook();
+        int result = phoneBook.add("Nik","12345678");
+        assertEquals(1,result);
+    }
 }
